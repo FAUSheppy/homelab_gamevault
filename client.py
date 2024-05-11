7dcc6a4245a4485b82ec53661474fb166dd38c13
Yannik Schmidt <yannik.schmidt@potaris.de>
2024-05-11 18:16:22 +0200

    fix: non-abs path in config

--- client.py
+++ client.py
@@ -14,7 +14,7 @@ customtkinter.set_default_color_theme("blue")
 
 app = customtkinter.CTk()
 
-app.geometry("1030x770")
+app.geometry("1230x770")
 last_geometry = app.winfo_geometry()
 
 buttons = []
